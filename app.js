55228798308bd1a56b50716e5a8e2bb60e4efea0
Shrey <shreyansh.chhalotra@gmail.com>
2022-12-29 02:02:15 -0600

    scroll changes

--- app.js
+++ app.js
@@ -5,3 +5,30 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 }); 
+
+const menuLink1 = document.querySelector('.navbar__item1');
+
+menuLink1.addEventListener('click', function() {
+    window.scrollTo({
+        top: 1100,
+        behavior: 'smooth'
+      });
+}); 
+
+const menuLink2 = document.querySelector('.navbar__item2');
+
+menuLink2.addEventListener('click', function() {
+    window.scrollTo({
+        top: 1900,
+        behavior: 'smooth'
+      });
+}); 
+
+const menuLink3 = document.querySelector('.navbar__item3');
+
+menuLink3.addEventListener('click', function() {
+    window.scrollTo({
+        top: 2650,
+        behavior: 'smooth'
+      });
+}); 
